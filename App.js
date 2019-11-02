8e426ffee12588f75fae2e5a7ce4225d37f6fe74
Wasut Panyawiphat <wasut_pao@hotmail.com>
2019-11-02 14:49:28 +0700

    install expo font

--- App.js
+++ App.js
@@ -2,16 +2,41 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { Icon } from 'native-base';
+import { AppLoading } from 'expo';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 import HomeTab from './components/HomeTab';
 import SearchTab from './components/SearchTab';
 import UploadTab from './components/UploadTab';
 import ActivityTab from './components/ActivityTab';
 import ProfileTab from './components/ProfileTab';
 
-export default function App() {
-  return (
-    <AppContainer />
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true });
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
+
+    return (
+      <AppContainer />
+    );
+  }
 }
 
 const getTab = (tab, icon) => {
